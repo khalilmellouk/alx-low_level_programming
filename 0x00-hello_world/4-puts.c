aad630ac9ca1d923c824174d3af3ae61a62c4b12
khalil mellouk <khalilmellouk10@gmail.com>
2023-06-15 17:22:41 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a line using the puts function
+ *
+ * Return: 0 for successful execution
+ */
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
