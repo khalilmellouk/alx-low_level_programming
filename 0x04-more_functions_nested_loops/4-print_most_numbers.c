4713cb3ed8723b76b3296a5bf8d99c5259a5bed3
khalil mellouk <khalilmellouk10@gmail.com>
2023-06-22 19:23:59 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,24 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
+*/
 void print_most_numbers(void)
 {
-    int c;
+	int c;
 
-    for (c = 48; c < 58; c++)
-    {
-        if (c != 50)
-        {
-            if (c != 52)
-            {
-                _putchar( c );
-            }
-        }
-    }
-    _putchar('\n');
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar( c );
+			}
+		}
+	}
+	_putchar('\n');
 }
